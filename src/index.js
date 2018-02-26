0259ac6d579811ceaa685669af4a070c0bde8a5d
arka.j.biswas@gmail.com <ABISWA10@hallmark.com>
2018-02-26 16:10:50 -0600

    Made to Work with OS3

--- src/index.js
+++ src/index.js
@@ -2,16 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // using an ES6 transpiler, like babel
 import Image from 'react-image';
+import axios from 'axios';
 
 //import styles from '../css/w3.css';
 const logo = require('../Chan.png');
 
 var headingValue="This is A good Day";
+var quotes="Initial";
 class App extends React.Component {
     constructor(props) {
-        super(props);
+        super(props); this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
       }
       
+/*    componentDidMount() {
+        fetch("http://crudapp-os3data.a3c1.starter-us-west-1.openshiftapps.com/quotes")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result.items
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }*/
+      
+      /*componentDidMount() {
+        axios.get('http://crudapp-os3data.a3c1.starter-us-west-1.openshiftapps.com/quotes')
+          .then(res => {
+            this.setState({
+
+            quotes:res.data
+
+            });
+          });
+      }*/  
     render(){
         return(
         <div>
@@ -19,6 +57,7 @@ class App extends React.Component {
         <p>This content is rendered through React JSX</p>      
         <p>{headingValue}</p>
         <img src={logo}/>
+        {quotes}
         </div>
         )
     }
